bd02ffd9e4570c8aa5c65bf2a7d58fdc7039d0d0
luigi.gentile <luigi.gentile2@gmail.com>
2021-05-23 19:26:14 +0200

    database su heroku

--- cm/settings.py
+++ cm/settings.py
@@ -111,14 +111,26 @@ WSGI_APPLICATION = 'cm.wsgi.application'
 # }
 # 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#         'USER': 'postgres',
+#         'PASSWORD': 'filippo92',
+#         'NAME': 'circuit_mind',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '127.0.0.1',
+        'HOST': 'ec2-3-233-7-12.compute-1.amazonaws.com',
         'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': 'filippo92',
-        'NAME': 'circuit_mind',
+        'USER': 'vqfcacpiqszmgv',
+        'PASSWORD': 'ba8f3e356b063be412f74bfc449483b1d00c828329ba8255626b90907e3a6abc',
+        'NAME': 'del4asn0dhmi9s',
     }
 }
 
